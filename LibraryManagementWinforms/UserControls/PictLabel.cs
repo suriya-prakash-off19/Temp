f0a1528b2ed5bca04bec9b0618e4642ac9ad4922
suriya-prakash-off19 <suriyaprakashoff19@gmail.com>
2024-09-09 13:50:21 +0530

    Added 2

---
+++ LibraryManagementWinforms/UserControls/PictLabel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserControls
+{
+    public partial class PictLabel : UserControl
+    {
+        public PictLabel()
+        {
+            InitializeComponent();
+        }
+        //[Category("PictureBox")]
+        //public Image Picture
+        //{
+        //    get => PictCon.Image;
+        //    set
+        //    {
+        //        PictCon.Image = value;
+        //    }
+        //}
+        //[Category("PictureBox")]
+        //public PictureBoxSizeMode SizeMode
+        //{
+        //    get => PictCon.SizeMode;
+        //    set
+        //    {
+        //        PictCon.SizeMode = value;
+        //    }
+        //}
+        //public override string Text
+        //{
+        //    get => LabelCon.Text;
+        //    set
+        //    {
+        //        LabelCon.Text = value;
+        //    }
+        //}
+
+        //public override Font Font
+        //{
+        //    get => LabelCon.Font;
+        //    set
+        //    {
+        //        LabelCon.Font = value;
+        //    }
+        //}
+
+        //public override Color BackColor
+        //{
+        //    get => LabelCon.BackColor;
+        //    set
+        //    {
+        //        LabelCon.BackColor = value;
+        //        PictCon.BackColor = value;
+        //    }
+        //}
+      }
+}
